7a07e8c85ea4596413f417be340764e4211c65d4
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2025-08-05 06:33:14 +0530

    thisKeyword.cpp

--- thisKeyword.cpp
+++ thisKeyword.cpp
@@ -1,24 +1,37 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class Cricketer{
 public:
 	string name;
 	int runs;
+	float avg;
 	
-	Cricketer(string name,int runs){
+	Cricketer(string name,int runs,float avg){
 		this->name=name;
 		this->runs=runs;
+		this->avg=avg;
+	}
+	
+	void print(){
+	cout<<name<<" "<<runs<<" "<<avg<<endl;
+	}
+	
+	int matches(){
+		return runs/avg;
 	}
 };
+
+
+
 int main(){
+	Cricketer c1("Virat Kohli",30000,55.2);
 	
-	Cricketer c1("Virat Kohli",30000);
-	
-	Cricketer c2("Rohit Sharma",18000);
+	Cricketer c2("Rohit Sharma",18000,47.8);
 	
 	
-	cout<<c1.name<<" "<<c1.runs<<endl;
-	cout<<c2.name<<" "<<c2.runs;
-	
+	c1.print();
+	c2.print();
+	cout<<c1.matches()<<endl;
+	cout<<c2.matches();
 }
